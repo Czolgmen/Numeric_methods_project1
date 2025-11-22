064bacd28f9b0b73ba7416cc6f4839c33682dc6c
Czolgmen <arturczolg@gmail.com>
2025-11-22 04:48:09 +0100

    2nd version done

--- numtestIntegrateDiskTrapezoid.m
+++ numtestIntegrateDiskTrapezoid.m
@@ -10,6 +10,7 @@ function numtestIntegrateDiskTrapezoid()
 %
 %   Uruchom:
 %       >> numtestIntegrateDiskTrapezoid
+    MAKE_PLOTS = true;  % <- gdy true, rysuje wykresy zbieżności
 
     N_list   = [4 8 16 32 64];
     mappings = {'linear','sqrt', 'm1', 'm3'};
@@ -18,7 +19,7 @@ function numtestIntegrateDiskTrapezoid()
     myPrint('===== NUMERYCZNE TESTY integrateDiskTrapezoid =====\n\n');
 
     numtestTables(tests, mappings, N_list);
-    numtestConvergence(tests, mappings);
+    numtestConvergence(tests, mappings, MAKE_PLOTS);
 
     myPrint('\nKoniec numerycznych testów.\n');
 end
@@ -38,6 +39,10 @@ function tests = buildNumtestTests()
     tests(end+1).name    = 'f(x,y) = x';
     tests(end  ).f       = @(x,y) x;
     tests(end  ).I_exact = 0;
+
+    tests(end+1).name    = 'f(x,y) = |x|';
+    tests(end  ).f       = @(x,y) abs(x);
+    tests(end  ).I_exact = 4/3;
 
     % 3) f = y, integral = 0
     tests(end+1).name    = 'f(x,y) = y';
@@ -72,7 +77,7 @@ function numtestTables(tests, mappings, N_list)
         for m = 1:numel(mappings)
             mapping = mappings{m};
             myPrint('  Mapping: %s\n', mapping);
-            myPrint(['    %6s  %20s  %20s  %12s  %12s\n'], ...
+            myPrint('    %6s  %20s  %20s  %12s  %12s\n', ...
                 'N', 'I_num', '|I_num - I_exact|', ...
                 'err(est)', 'time[s]');
 
@@ -101,24 +106,27 @@ end
 %---------------------------------------------
 % 2) Analiza zbieżności błędu i ratio
 %---------------------------------------------
-function numtestConvergence(tests, mappings)
+function numtestConvergence(tests, mappings, makePlots)
     N_conv = 2.^(1:12);   % 2,4,8,...,4096
 
     for t = 1:numel(tests)
-        myPrint(['*** Analiza zbieżności błędu dla %s ***\n'], ...
+        myPrint('*** Analiza zbieżności błędu dla %s ***\n', ...
             tests(t).name);
 
         for m = 1:numel(mappings)
             mapping = mappings{m};
             myPrint('  Mapping: %s\n', mapping);
-            myPrint(['    %6s  %20s  %20s  %16s\n'], ...
+            myPrint('    %6s  %20s  %20s  %16s\n', ...
                 'N', '|I_num - I_exact|', 'err(est)', ...
                 'ratio(prev/cur)');
 
-            prev_err = NaN;
+            prev_err      = NaN;
+            abs_err_list  = zeros(size(N_conv));
+            err_est_list  = zeros(size(N_conv));
+            ratio_list    = NaN(size(N_conv));
 
             for k = 1:numel(N_conv)
-                N = N_conv(k);
+                N  = N_conv(k);
                 Nu = N;
                 Nv = N;
 
@@ -126,11 +134,15 @@ function numtestConvergence(tests, mappings)
                     tests(t).f, Nu, Nv, mapping);
                 abs_err = abs(I_num - tests(t).I_exact);
 
+                abs_err_list(k) = abs_err;
+                err_est_list(k) = err_est;
+
                 ratioStr = '-';
                 if ~isnan(prev_err) && isfinite(prev_err) ...
                         && isfinite(abs_err) ...
                         && prev_err > 0 && abs_err > 0
                     ratio = prev_err / abs_err;
+                    ratio_list(k) = ratio;
                     ratioStr = sprintf('%.4f', ratio);
                 end
 
@@ -142,6 +154,13 @@ function numtestConvergence(tests, mappings)
             end
 
             myPrint('\n');
+
+            % Wykres zbieżności dla danej funkcji i mapowania
+            if makePlots
+                makeConvergencePlot(N_conv, abs_err_list, ...
+                    err_est_list, ratio_list, tests(t).name, ...
+                    mapping);
+            end
 
             if m < numel(mappings)
                 pauseScreen();
@@ -156,6 +175,7 @@ function numtestConvergence(tests, mappings)
     end
 end
 
+
 %---------------------------------------------
 % Pomocnicze drukowanie / pauza (wspólne)
 %---------------------------------------------
@@ -166,3 +186,38 @@ end
 function pauseScreen()
     input('--- Naciśnij Enter, aby kontynuować ---','s');
 end
+
+function makeConvergencePlot(N_vals, abs_err_list, ...
+    err_est_list, ratio_list, fname, mapping)
+
+    % Skala log_2 na osi X:
+    x_vals = log2(N_vals);   % np. dla 2,4,8,... wyjdzie 1,2,3,...
+
+    figure;
+    yyaxis left;
+    % Błędy w skali logarytmicznej (Y), X w skali log_2(N)
+    semilogy(x_vals, abs_err_list, '-o');  % błąd absolutny
+    hold on;
+    semilogy(x_vals, err_est_list, '-x');  % błąd estymowany
+    ylabel('Błąd (skala log Y)');
+
+    yyaxis right;
+    plot(x_vals, ratio_list, '-s');        % ratio w skali liniowej po Y
+    ylabel('ratio(prev/cur)');
+
+    % Oś X – opisujemy jako log_2(N)
+    xlabel('log_2(N)');
+
+    % Opcjonalnie: ładne ticki (tutaj 1,2,3,... odpowiadające 2,4,8,...)
+    xticks(x_vals);
+    xticklabels(arrayfun(@(v) sprintf('%.0f', v), x_vals, ...
+        'UniformOutput', false));
+
+    title(sprintf('Zbieżność błędu: %s, mapping = %s', ...
+        fname, mapping), 'Interpreter', 'none');
+    legend({'|I_{num} - I_{exact}|', 'err(est)', ...
+        'ratio(prev/cur)'}, 'Location', 'best');
+
+    grid on;
+end
+
--- testIntegrateDiskTrapezoid.m
+++ testIntegrateDiskTrapezoid.m
@@ -4,6 +4,9 @@ function testIntegrateDiskTrapezoid()
 %
 % TESTINTEGRATEDISKTRAPEZOID
 %   Testy poprawności działania funkcji integrateDiskTrapezoid.
+%   Dla każdej funkcji testowej pokazujemy wyniki dla rosnących
+%   rozmiarów siatki (Nu, Nv), gdzie w każdej iteracji Nu i Nv
+%   są mnożone przez 2.
 
     % zresetowanie licznika linii w myPrint
     myPrint(-1, '');
@@ -17,21 +20,29 @@ function testIntegrateDiskTrapezoid()
         t = tests(k);
 
         myPrint(1, 'Test %d:\n', k);
-        myPrint(1, '  Funkcja:   %s\n', t.desc);
-        myPrint(1, ['  Parametry: Nu = %d, Nv = %d, ' ...
-                    'mapping = %s\n'], ...
-            t.Nu, t.Nv, t.mapping);
-
-        I_num = integrateDiskTrapezoid(t.f, t.Nu, t.Nv, t.mapping);
-
-        myPrint(1, ['  Oczekiwana wartość I_exact: ' ...
+        myPrint(1, '  Funkcja : %s\n', t.desc);
+        myPrint(1, '  Mapping : %s\n', t.mapping);
+        myPrint(1, ['  Wartość dokładna I_exact: ' ...
                     '%.15g\n'], t.I_exact);
-        myPrint(1, ['  Wynik obliczony I_num     : ' ...
-                    '%.15g\n'], I_num);
+        myPrint(2, ['  Siatka startowa: Nu = %d, Nv = %d, ' ...
+                    'liczba poziomów: %d\n\n'], ...
+                    t.N0, t.N0, t.nLevels);
 
-        diff_val = abs(I_num - t.I_exact);
-        myPrint(2, ['  Różnica |I_num - I_exact| : ' ...
-                    '%.15g\n\n'], diff_val);
+        for lev = 0:(t.nLevels - 1)
+            Nu = t.N0 * 2^lev;
+            Nv = Nu;
+
+            I_num = integrateDiskTrapezoid(t.f, Nu, Nv, t.mapping);
+            diff_val = abs(I_num - t.I_exact);
+
+            myPrint(1, '    Nu = %5d, Nv = %5d:\n', Nu, Nv);
+            myPrint(1, ['      I_num                  = ' ...
+                        '%.15g\n'], I_num);
+            myPrint(2, ['      |I_num - I_exact|       = ' ...
+                        '%.15g\n\n'], diff_val);
+        end
+
+        myPrint(1, '----------------------------------------\n\n');
     end
 
     myPrint(1, ['===== Koniec testu poprawności ' ...
@@ -40,58 +51,111 @@ end
 
 %------------------------------------------------------------
 % Definicja prostych testów z dokładnie znaną całką
+% Każdy test ma:
+%   - N0      : rozmiar siatki startowej (Nu = Nv = N0)
+%   - nLevels : ile razy mnożymy N przez 2
 %------------------------------------------------------------
 function tests = buildCorrectnessTests()
     tests = struct([]);
 
-    % 1) f = 1  -> całka = pi
-    tests(end+1).desc    = 'f(x,y) = 1';
+    % 1) f = 1  -> całka = pi, mapping = linear
+    tests(end+1).desc    = 'f(x,y) = 1 (linear)';
     tests(end  ).f       = @(x,y) ones(size(x));
     tests(end  ).I_exact = pi;
-    tests(end  ).Nu      = 8;
-    tests(end  ).Nv      = 8;
     tests(end  ).mapping = 'linear';
+    tests(end  ).N0      = 4;    % 4, 8, 16, 32, 64
+    tests(end  ).nLevels = 5;
 
-    % 2) f = 1, ten sam test dla mapping = sqrt
-    tests(end+1).desc    = 'f(x,y) = 1';
+    % 2) f = 1  -> całka = pi, mapping = sqrt
+    tests(end+1).desc    = 'f(x,y) = 1 (sqrt)';
     tests(end  ).f       = @(x,y) ones(size(x));
     tests(end  ).I_exact = pi;
-    tests(end  ).Nu      = 8;
-    tests(end  ).Nv      = 8;
     tests(end  ).mapping = 'sqrt';
+    tests(end  ).N0      = 4;
+    tests(end  ).nLevels = 5;
 
-    % 3) f = x   -> całka = 0 (symetria)
-    tests(end+1).desc    = 'f(x,y) = x';
+    % 3) f = 1  -> całka = pi, mapping = m3
+    tests(end+1).desc    = 'f(x,y) = 1 (m3)';
+    tests(end  ).f       = @(x,y) ones(size(x));
+    tests(end  ).I_exact = pi;
+    tests(end  ).mapping = 'm3';
+    tests(end  ).N0      = 4;
+    tests(end  ).nLevels = 5;
+
+    % 4) f = x   -> całka = 0 (symetria), mapping = linear
+    tests(end+1).desc    = 'f(x,y) = x (linear)';
     tests(end  ).f       = @(x,y) x;
     tests(end  ).I_exact = 0;
-    tests(end  ).Nu      = 8;
-    tests(end  ).Nv      = 8;
     tests(end  ).mapping = 'linear';
+    tests(end  ).N0      = 4;
+    tests(end  ).nLevels = 5;
 
-    % 4) f = y   -> całka = 0 (symetria)
-    tests(end+1).desc    = 'f(x,y) = y';
+    % 5) f = |x| -> całka = 4/3, mapping = linear
+    tests(end+1).desc    = 'f(x,y) = |x| (linear)';
+    tests(end  ).f       = @(x,y) abs(x);
+    tests(end  ).I_exact = 4/3;
+    tests(end  ).mapping = 'linear';
+    tests(end  ).N0      = 16;   % 16, 32, 64, 128, 256, 512
+    tests(end  ).nLevels = 6;
+
+    % 6) f = y   -> całka = 0 (symetria), mapping = sqrt
+    tests(end+1).desc    = 'f(x,y) = y (sqrt)';
     tests(end  ).f       = @(x,y) y;
     tests(end  ).I_exact = 0;
-    tests(end  ).Nu      = 8;
-    tests(end  ).Nv      = 8;
     tests(end  ).mapping = 'sqrt';
+    tests(end  ).N0      = 4;
+    tests(end  ).nLevels = 5;
 
-    % 5) f = x^2 + y^2 -> całka = pi/2
-    tests(end+1).desc    = 'f(x,y) = x^2 + y^2';
+    % 7) f = x^2 + y^2 -> całka = pi/2, mapping = linear
+    tests(end+1).desc    = 'f(x,y) = x^2 + y^2 (linear)';
     tests(end  ).f       = @(x,y) x.^2 + y.^2;
     tests(end  ).I_exact = pi/2;
-    tests(end  ).Nu      = 16;
-    tests(end  ).Nv      = 16;
     tests(end  ).mapping = 'linear';
+    tests(end  ).N0      = 4;
+    tests(end  ).nLevels = 5;
 
-    % 6) f = x^2 + y^2 -> całka = pi/2 (mapping sqrt)
-    tests(end+1).desc    = 'f(x,y) = x^2 + y^2';
+    % 8) f = x^2 + y^2 -> całka = pi/2, mapping = sqrt
+    tests(end+1).desc    = 'f(x,y) = x^2 + y^2 (sqrt)';
     tests(end  ).f       = @(x,y) x.^2 + y.^2;
     tests(end  ).I_exact = pi/2;
-    tests(end  ).Nu      = 16;
-    tests(end  ).Nv      = 16;
     tests(end  ).mapping = 'sqrt';
+    tests(end  ).N0      = 4;
+    tests(end  ).nLevels = 5;
+
+    % 9) f = x^2 + y^2 -> całka = pi/2, mapping = m3
+    tests(end+1).desc    = 'f(x,y) = x^2 + y^2 (m3)';
+    tests(end  ).f       = @(x,y) x.^2 + y.^2;
+    tests(end  ).I_exact = pi/2;
+    tests(end  ).mapping = 'm3';
+    tests(end  ).N0      = 4;
+    tests(end  ).nLevels = 5;
+
+    % 10) f = x^2 -> całka = pi/4, mapping = linear
+    tests(end+1).desc    = 'f(x,y) = x^2 (linear)';
+    tests(end  ).f       = @(x,y) x.^2;
+    tests(end  ).I_exact = pi/4;
+    tests(end  ).mapping = 'linear';
+    tests(end  ).N0      = 4;
+    tests(end  ).nLevels = 5;
+
+    % 11) f = x^2 -> całka = pi/4, mapping = sqrt
+    tests(end+1).desc    = 'f(x,y) = x^2 (sqrt)';
+    tests(end  ).f       = @(x,y) x.^2;
+    tests(end  ).I_exact = pi/4;
+    tests(end  ).mapping = 'sqrt';
+    tests(end  ).N0      = 4;
+    tests(end  ).nLevels = 5;
+
+    % 12) f = x^2 -> całka = pi/4, mapping = m3
+    tests(end+1).desc    = 'f(x,y) = x^2 (m3)';
+    tests(end  ).f       = @(x,y) x.^2;
+    tests(end  ).I_exact = pi/4;
+    tests(end  ).mapping = 'm3';
+    tests(end  ).N0      = 4;
+    tests(end  ).nLevels = 5;
+
 end
+
 
 %------------------------------------------------------------
 % Pomocnicza funkcja wypisująca z liczeniem linii
